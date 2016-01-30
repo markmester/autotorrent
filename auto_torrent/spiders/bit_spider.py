05fc21c4e986c8a749bc7bee160072b27f8078ae
MarkMester <mmester6016@gmail.com>
2016-01-29 21:50:51 -0500

    fixed bug in episdoe/season scrapys

--- auto_torrent/spiders/bit_spider.py
+++ auto_torrent/spiders/bit_spider.py
@@ -10,7 +10,7 @@ class PirateSpider(scrapy.Spider):
   name = 'pirate'
 
   def __init__(self, base_url='https://thepiratebay.se/search/',
-               search_term='modern family', season='', episode=''):
+               search_term='test', season='', episode=''):
     self.base_url = base_url
     self.season = season
     self.episode = episode
@@ -40,26 +40,26 @@ class PirateSpider(scrapy.Spider):
         item['title'] = ''
 
       #get season
+      season = ''
       try:
-        season_abr = re.search('E(\d.*?)\D', str(item['title'])).group(1)
-        season_complete = re.search('Season.*?(\d.*?)\D', str(item['title'])).group(1)
+        season = re.search('E(\d.*?)\D', str(item['title'])).group(1)
+        season = re.search('Season.*?(\d.*?)\D', str(item['title'])).group(1)
       except AttributeError:
         pass
 
-      if season_abr:
-        item['season'] = season_abr
-      elif season_complete:
-        item['season'] = season_complete
+      if season != '':
+        item['season'] = season
       else:
         item['season'] = ''
 
       #get episode
+      episode = ''
       try:
         episode = re.search('E(\d.*?)\D', str(item['title'])).group(1)
       except AttributeError:
         pass
 
-      if episode:
+      if episode != '':
         item['episode'] = episode
       else:
         item['episode'] = ''
@@ -91,23 +91,9 @@ class PirateSpider(scrapy.Spider):
     # download torrent
     # self.download_torrent(magnet_links[1])
 
-
   def download_torrent(self, magnet_link):
     tc = transmissionrpc.Client('localhost', port=9091) # connect to transmission daemon
-
     tc.add_torrent(magnet_link)
-
-
-    def filter_magnet_links(self, links):
-      pass
-
-
-  def remove_torrent(self, torrent):
-    pass
-
-
-  def move_torrent(self, file):
-    pass
 
 
   def format_search_term(self, season, episode):
@@ -125,3 +111,15 @@ class PirateSpider(scrapy.Spider):
         pass
 
       return 'S' + season + 'E' + episode
+
+
+  def filter_magnet_links(self, links):
+    pass
+
+
+  def remove_torrent(self, torrent):
+    pass
+
+
+  def move_torrent(self, file):
+    pass
